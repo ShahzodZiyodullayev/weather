971d1caf577af921e631a65c6a5db8fbbd164ebf
ShahzodZiyodullayev <sshahzod5@gmail.com>
2022-09-14 16:12:33 +0500

    Dizaynda o'zgarish bo'ldi. Hamma componentlar card'larga ajratildi

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Grid } from "@mui/material";
 import LeftSide from "../LeftSide/LeftSide";
 import RightSide from "../RightSide/RightSide";
+import { Grid } from "@mui/material";
 
 function Home(props) {
-  const { currentLocation, setCurrentLocation, select } = props;
+  const { currentLocation } = props;
 
   return (
-    <Grid container>
-      <LeftSide currentLocation={currentLocation} />
-      <RightSide />
+    <Grid padding={3} height="100vh" backgroundColor="#282828">
+      <Grid container columnSpacing={3} height="100%">
+        <LeftSide currentLocation={currentLocation} />
+        <RightSide />
+      </Grid>
     </Grid>
   );
 }
