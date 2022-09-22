4ab43e7c31de8663692400756f5df7b7a23e2819
ShahzodZiyodullayev <sshahzod5@gmail.com>
2022-09-22 23:28:21 +0500

    background o'zgartirildi

--- src/components/InfoCard/InfoCard.js
+++ src/components/InfoCard/InfoCard.js
@@ -5,13 +5,17 @@ import "./styles.css";
 function InfoCard({ icon, name, value }) {
   return (
     <Grid className="sunrise_and_sunset_left">
-      <Grid>{icon}</Grid>
+      <Grid className="weather-property_icon">{icon}</Grid>
       <Grid ml={1}>
         <Typography variant="body" className="weather-property_title">
           {name}
         </Typography>
         <br />
-        <Typography variant="body" className="weather-property_value">
+        <Typography
+          variant="body"
+          color="#E6E6E6"
+          className="weather-property_value"
+        >
           {value}
         </Typography>
       </Grid>
