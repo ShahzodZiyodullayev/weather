29dcf829fad25f0cf7d340c3f430afad9b206d54
ShahzodZiyodullayev <sshahzod5@gmail.com>
2022-09-29 00:42:18 +0500

    background pexels bilan integratsiya qilindi

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import LeftSide from "../LeftSide/LeftSide";
 import RightSide from "../RightSide/RightSide";
 import { Grid } from "@mui/material";
+import { useSelector } from "react-redux";
+import { createClient } from "pexels";
 import "./style.css";
 
 function Home(props) {
   const { currentLocation } = props;
+  const [photos, setPhotos] = useState("");
+  const current = useSelector((state) => state.current);
+
+  // if (current && current.weather) {
+  //   const client = createClient(
+  //     "563492ad6f917000010000014779d010e7dc4223b3ddb3fa3015e28e",
+  //     // "563492ad6f91700001000001b6fde700f576471ca84ed7fd449385e5",
+  //   );
+  //   const query = current.weather[0].description;
+
+  //   client.photos
+  //     .search({ query, per_page: 1 })
+  //     .then((photos) => setPhotos(photos.photos[0].src.original));
+  // }
 
   return (
-    <Grid padding={3} height="100vh" backgroundColor="#282828" className="home">
+    <Grid
+      padding={3}
+      className="home"
+      sx={{
+        backgroundImage: `url(${photos})`,
+      }}
+    >
       <Grid container columnSpacing={3} height="100%">
         <LeftSide currentLocation={currentLocation} />
         <RightSide />
